a01571dbbc965129a4ea89bc689a36d787d8c22c
Murilo Rocha <murilo.rocha@arenadunas.com.br>
2018-11-28 19:24:39 -0300

    Autenticação, migrations, models e seeds com factory

---
+++ database/migrations/2018_11_28_191205_create_funcionarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresas');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('telefone');
+            $table->string('cpf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
